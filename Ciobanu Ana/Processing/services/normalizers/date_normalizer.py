80bb43b06c88d5b342f7d30ee238c371674879ff
Ciobanu Ana <ciobanu.nica@bk.ru>
2024-11-10 23:57:20 +0200

    Clean comments

--- Ciobanu Ana/Processing/services/normalizers/date_normalizer.py
+++ Ciobanu Ana/Processing/services/normalizers/date_normalizer.py
@@ -1,31 +1,5 @@
-# from .normalizer_strategy import Normalizer
-# from datetime import datetime
-#
-# class DateNormalizer(Normalizer):
-#     """Normalization strategy for dates."""
-#
-#     def normalize(self, date_string):
-#         # Date formats that can occur
-#         date_formats = [
-#             "%Y-%m-%d",
-#             "%d/%m/%Y",
-#             "%B %d, %Y"
-#         ]
-#
-#         for date_format in date_formats:
-#             try:
-#                 # Try to parse the date with the current format
-#                 parsed_date = datetime.strptime(date_string, date_format)
-#                 # Format the date as dd/mm/yyyy
-#                 return parsed_date.strftime("%d/%m/%Y")
-#             except ValueError:
-#                 continue  # Try the next format if the current one fails
-#
-#         # None of the formats worked, raise a ValueError
-#         raise ValueError(f"Invalid date format: {date_string}")
 from .normalizer_strategy import Normalizer
 from datetime import datetime
-import aspectlib
 from Processing.utils.aop_logging import log_aspect, exception_handling_aspect
 
 class DateNormalizer(Normalizer):
