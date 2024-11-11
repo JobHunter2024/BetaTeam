4ebb8354c164a601b62206b97e530e4338a667de
Ciobanu Ana <ciobanu.nica@bk.ru>
2024-11-11 22:51:21 +0200

    Company Name Normalizer

--- Ciobanu Ana/Processing/services/normalizers/company_name_normalizer.py
+++ Ciobanu Ana/Processing/services/normalizers/company_name_normalizer.py
@@ -1,7 +1,11 @@
 from normalizer_strategy import Normalizer
+import re
 
 class CompanyNameNormalizer(Normalizer):
     """Normalization strategy for company names."""
 
     def normalize(self, company_name):
-        return
+        # Remove common suffixes
+        company_name = re.sub(r"\b(inc|llc|ltd|corp|co|company)\b", "", company_name)
+        company_name = company_name.title()
+        return company_name
